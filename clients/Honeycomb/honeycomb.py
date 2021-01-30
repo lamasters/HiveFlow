40d883daeacbd6638df34d52cb3f6e7cb1002773
Liam Masters <liam.masters3@gmail.com>
2021-01-30 13:27:02 -0500

    Create idle detection algorithm

--- clients/Honeycomb/honeycomb.py
+++ clients/Honeycomb/honeycomb.py
@@ -2,13 +2,14 @@ import time
 import face_detection as fr
 import cv2 as cv
 import numpy as np
-from skimage.measure import compare_ssim
+from skimage.metrics import structural_similarity as ssim
 
 cap = cv.VideoSource(0)
 in_home = False
 _, bg_filter = cap.read()
 bg_shape = (0,0)
 frame_time = time.time()
+bg_time = time.time()
 
 # Generate a unique id for a user
 def generate_uid():
@@ -23,6 +24,7 @@ def process_image(frame):
     global bg_shape
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     gray = cv.resize(gray, bg_shape)
+    gray = cv.GaussianBlur(gray, (13, 13), 0)
     return gray
 
 # Get an idle image to watch for
@@ -34,15 +36,24 @@ def create_filter():
     return 0
 
 # Passively detect movement
-def idle():
+def idle(change_count):
     global bg_filter, frame_time
     
-    if time.time() - frame_time > 1/2:
+    if change_count = 0:
+        frame_wait = 1/2
+    else:
+        frame_wait = 1/30
+
+    if time.time() - frame_time > frame_wait:
         _, frame = cap.read()
         frame = process_image(frame)
-        score, diff_map = compare_ssim(frame, bg_filter, full=True)
-        if score < 0.5:
+        score, diff_map = ssim(frame, bg_filter, full=True)
+        if score < 0.75:
+            bg_time = time.time()
             return False
+        elif time.time() - bg_time > 60:
+            create_filter()
+            bg_time = time.time()
 
     return True
 
@@ -59,14 +70,23 @@ def greet():
     return 0
 
 create_filter()
+change_count = 0
 
 while True:
-    if in_home or idle():
+    if in_home:
         continue
-
-    faces = detect()
-
-    if len(faces) == 0:
-        scan()
+    
+    if idle(change_count):
+        change_count = 0
+        continue
     else:
-        in_home = True
+        change_count += 1
+
+    if change_count > 30:
+        faces = detect()
+
+        if len(faces) == 0:
+            scan()
+        else:
+            in_home = True
+            greet(faces)
